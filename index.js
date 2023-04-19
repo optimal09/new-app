d745f778f30a7a4af5bf96781f5d7f57bcdfe711
Sam <111366223+optimal09@users.noreply.github.com>
2023-04-19 14:26:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
+  res.write('<div style="text-align: center">');
   res.write('<h1>HELLO WORLD AND WELCOME TO WSD.</h1>');
   res.write('<p>TODAY IS A VERY GOOD DAY FOR US ALL. THANK YOU</p>');
+  res.write('</div>');
   res.end();
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
